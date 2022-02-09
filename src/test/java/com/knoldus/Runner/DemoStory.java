a97d3202b80e9a1da1e752932cb21938a9c5e2f7
Sparsh <sparsh.bhardwaj@knoldus.com>
2022-02-09 13:26:09 +0530

    Added project for BDD with Rest Assured and JBehave

---
+++ src/test/java/com/knoldus/Runner/DemoStory.java
@@ -0,0 +1,34 @@
+package com.knoldus.Runner;
+import com.knoldus.steps.DemoSteps;
+import io.tapack.allure.jbehave.AllureReporter;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import java.util.Arrays;
+import java.util.List;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.CONSOLE;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.HTML;
+public class DemoStory extends JUnitStories {
+    @Override
+    public Configuration configuration() {
+        if (super.hasConfiguration()) {
+            return super.configuration();
+        }
+        return new MostUsefulConfiguration()
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withDefaultFormats()
+                        .withFormats(CONSOLE, HTML)
+                        .withReporters(new AllureReporter()));
+    }
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new DemoSteps());
+    }
+    @Override
+    protected List<String> storyPaths() {
+        return Arrays.asList("Demo.story");
+    }
+}
